f362395a650106db0c2cd4a7266a04d8463a5e87
PARSA-00-55 <parsa02345@gmail.com>
2022-09-03 09:09:05 +0600

    nav-bar and menu-bar added

--- language/js/index.js
+++ language/js/index.js
@@ -1,1 +1,29 @@
-console.log('hello')
+//menu-bar item
+const loadMenubar = async () => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/news/categories`
+  );
+  const data = await res.json();
+  return data;
+};
+//menu-bar item list
+const addMenuItem = async () => {
+  const data = await loadMenubar();
+  const menuItem = data.data.news_category;
+  const menu = document.getElementById("menu-bar");
+
+  for (const item of menuItem) {
+    console.log(item);
+    const li = document.createElement("li");
+    li.classList.add("menu-bar-list-item");
+    li.innerHTML = `<button onclick="news()">${item.category_name}</button>`;
+    menu.appendChild(li);
+  }
+};
+
+const news = () =>{
+  console.log('hello')
+}
+
+addMenuItem();
+// loadMenubar()
