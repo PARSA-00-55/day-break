57254a260689dcec823507ae88bbc3cf568a1d4d
PARSA-00-55 <parsa02345@gmail.com>
2022-09-04 01:00:57 +0600

    news feed card added

--- language/js/index.js
+++ language/js/index.js
@@ -1,10 +1,14 @@
 //menu-bar item
 const loadMenubar = async () => {
-  const res = await fetch(
-    `https://openapi.programming-hero.com/api/news/categories`
-  );
-  const data = await res.json();
-  displayMenu(data.data.news_category);
+  try{
+    const res = await fetch(
+      `https://openapi.programming-hero.com/api/news/categories`
+    );
+    const data = await res.json();
+    displayMenu(data.data.news_category);
+  }catch(error){
+    console.log(error);
+  }
   // return data;
 };
 
@@ -23,47 +27,49 @@ const displayMenu = news =>{
 
 const newsIdLoad = async(newsId) =>{
   const url = `https://openapi.programming-hero.com/api/news/category/${newsId}`
+  try{
   const res = await fetch(url);
   const data = await res.json();
-
   newsCard(data.data)
+  }catch(error){
+    console.log(error)
+  }
+
+
+  console.log(data.data.length)
 }
 
 const newsCard = async(items) =>{
   const newsFeed = document.getElementById('news-feed');
-  newsFeed.innerHTML = '';
+  newsFeed.innerHTML = ``;
   for(const item of items){
     console.log(item)
     const newsDiv = document.createElement("div");
     newsDiv.innerHTML = `
-    <div class="card lg:card-side bg-base-200 shadow-xl">
-                    <figure><img src="https://placeimg.com/400/400/arch" alt="Album"></figure>
+    <div class="card lg:card-side bg-base-200 shadow-xl my-5">
+                    <figure><img src="${item.thumbnail_url}" alt="Album"></figure>
                     <div class="card-body">
-                        <h2 class="card-title">New album is released!</h2>
-                        <p>Click the button to listen on Spotiwhy app. Lorem ipsum dolor, sit amet consectetur adipisicing
-                            elit. Illum praesentium debitis tempore explicabo error, quia repudiandae quibusdam voluptates
-                            eligendi odio totam, ad quod quidem earum ducimus sequi sit fuga ea iusto ab fugiat! Distinctio
-                            eum, nesciunt quisquam ab omnis sunt eos molestiae beatae exercitationem possimus excepturi
-                            itaque animi cupiditate? Delectus?</p>
+                        <h2 class="card-title">${item.title}</h2>
+                        <p>${item.details.slice(0, 400) ? item.details.slice(0, 400)+"..." : ""}</p>
                         <!-- card bottom -->
                         <div class="card-actions flex justify-between items-center">
                             <!-- author section -->
                             <div class="flex gap-4">
                                 <div class="avatar">
                                     <div class="w-14 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                        <img src="https://placeimg.com/192/192/people" />
+                                        <img src="${item.author.img}" />
                                     </div>
                                 </div>
                                 <!-- author info -->
                                 <div>
-                                    <h4>jane Cooper</h4>
-                                    <p>Jan, 10, 2022</p>
+                                    <h4>${item.author.name}</h4>
+                                    <p>${item.author.published_date}</p>
                                 </div>
                             </div>
                             <!-- views section -->
                             <div class="flex justify-between items-center gap-1">
                                 <i class="fa-solid fa-eye"></i>
-                                <p>400 view</p>
+                                <p>${item.total_view} viewed</p>
                             </div>
                             <!-- rateing -->
                             <div class="rating rating-md">
